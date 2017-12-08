41673e5ecf8c6dfd44df80dc406c980d013d4c2e
thien ban BUI <thienban91@hotmail.fr>
2017-12-08 11:47:02 +0100

    commit 1

---
+++ src/main/java/test/TestJpa.java
@@ -0,0 +1,17 @@
+package test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+		EntityManager em = entityManagerFactory.createEntityManager();
+		em.close();
+		entityManagerFactory.close();
+	}
+	
+}

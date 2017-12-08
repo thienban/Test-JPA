b17a087e8b99a95c5a12f6d744f4f3b7b8fa6479
thien ban BUI <thienban91@hotmail.fr>
2017-12-08 15:51:27 +0100

    tp3

--- src/main/java/entite/Article.java
+++ src/main/java/entite/Article.java
@@ -1,4 +1,4 @@
-package test;
+package entite;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,9 +14,9 @@ public class Article {
 	private String ref;
 	@Column(name = "DESIGNATION", length = 30, nullable = false)
 	private String designation;
-	@Column(name = "Prix", length = 30, nullable = false)
+	@Column(name = "Prix", nullable = false)
 	private float prix;
-	@Column(name = "ID_FOU", length = 30, nullable = false)
+	@Column(name = "ID_FOU", nullable = false)
 	private Integer id_fou;
 	public Article() {
 	}
--- src/main/java/test/TestJpa.java
+++ src/main/java/test/TestJpa.java
@@ -8,17 +8,24 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
-public class TestJpa {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import entite.Article;
+
+public class TestJpa {
+	private static final Logger LOG = LoggerFactory.getLogger(TestJpa.class);
+	 public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager em = entityManagerFactory.createEntityManager();
 		
 		
-		TypedQuery<Article> query2 = em.createQuery("select a from article", Article.class);
+		TypedQuery<Article> query2 = em.createQuery("select a from Article a", Article.class);
 		if (query2 != null) {
 			List<Article> articles = query2.getResultList();
+			LOG.info("le nombre article est"+articles.size());
+			
+			
 		}
 		
 		em.close();
